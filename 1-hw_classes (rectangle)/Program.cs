f18a05c44cbaa487b7a8393c166467c451323002
Ivan <163324607+sa1ntMercury@users.noreply.github.com>
2024-04-21 14:45:12 +0300

    Rectangle

---
+++ 1-hw_classes (rectangle)/Program.cs
@@ -0,0 +1,10 @@
+﻿using _1_hw_classes__rectangle_;
+
+Console.WriteLine("Input first side lenght:");
+double side1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Input second side lenght:");
+double side2 = double.Parse(Console.ReadLine());
+
+Rectangle rectangle = new Rectangle(side1, side2);
+
+Console.WriteLine($"Area = {rectangle.Area};\tPerimeter = {rectangle.Perimeter}");
