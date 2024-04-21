dcbc18cd3753283afd4d1574b14ee3da75edc1a3
Ivan <163324607+sa1ntMercury@users.noreply.github.com>
2024-04-21 14:48:03 +0300

    Add files via upload

---
+++ 1-hw_classes (rectangle)/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace _1_hw_classes__rectangle_
+{
+    public class Rectangle
+    {
+        private double _side1;
+        private double _side2;
+
+        public double Area { get => AreaCalculator(); }
+        public double Perimeter { get => PerimeterCalculator(); }
+
+        public Rectangle(double side1, double side2)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+
+        private double AreaCalculator()
+        {
+            return _side1 * _side2;
+        }
+
+        private double PerimeterCalculator()
+        {
+            return 2 * (_side1 + _side2);
+        }
+    }
+}
+
