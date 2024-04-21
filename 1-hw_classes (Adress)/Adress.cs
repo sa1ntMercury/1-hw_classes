1740fd9c043ac245d3a20995dd2baa3d403d842d
Ivan <163324607+sa1ntMercury@users.noreply.github.com>
2024-04-21 14:49:06 +0300

    Add files via upload

---
+++ 1-hw_classes (Adress)/Adress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace _1_hw_classes__adress_
+{
+	public class Adress
+	{
+		private uint _index;
+		private string _country;
+		private string _city;
+		private string _street;
+		private ushort _house;
+		private ushort _apartment;
+
+		public uint Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public ushort House { get; set; }
+        public ushort Apartment { get; set; }
+
+        public override string ToString()
+        {
+            return $"Adress\nIndex: {Index}\nCountry: {Country}\nCity: {City}\nStreet: {Street}\nHouse: {House}\nApartment: {Apartment}";
+        }
+
+    }
+}
+
---
+++ 1-hw_classes (Adress)/Program.cs
@@ -0,0 +1,12 @@
+﻿using _1_hw_classes__adress_;
+
+Adress adress = new Adress();
+
+adress.Index = 66000;
+adress.Country = "Ukraine";
+adress.City = "Odesa";
+adress.Street = "Nebesnoi Sotni";
+adress.House = 100;
+adress.Apartment = 44;
+
+Console.WriteLine(adress);
